825754c50f71828c7fe2f9d706911854d2255308
John Owen Danuarta <john.owen.d19@gmail.com>
2022-09-29 08:22:42 +0700

    Adding new getJumlahData function to return jumlah data and updating getMovie function to accomodate 2 types of inputs

--- class/movie.php
+++ class/movie.php
@@ -9,15 +9,28 @@ class movie extends orangtua
         parent::__construct();
     }
 
-    public function getMovie($keyword, $offset, $limit)
+    public function getMovie($keyword, $offset=null, $limit=null)
     {
-        $sql = "SELECT * FROM movie WHERE judul like ? LIMIT ?,?";
+        $sql = "SELECT * FROM movie WHERE judul like ?";
+        if (!is_null($offset) && !is_null($limit)) {
+            $sql .= " LIMIT ?, ?";
+        }
         $stmt = $this->mysqli->prepare($sql);
         $modified_keyword = "%" . $keyword . "%";
-        $stmt->bind_param('sii', $modified_keyword, $offset, $limit);
+        if (!is_null($offset)) {
+            $stmt->bind_param('sii', $modified_keyword, $offset, $limit);
+        } else {
+            $stmt->bind_param('s', $modified_keyword);
+        
+        }
         $stmt->execute();
         $res = $stmt->get_result();
 
         return $res;
+    }
+
+    public function getJumlahData($keyword) {
+        $res = $this->getMovie($keyword);
+        return $res->num_rows;
     }
 }
