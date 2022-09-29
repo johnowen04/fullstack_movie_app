e5d0368798e5f4ef53daa57d1a792c877683389a
John Owen Danuarta <john.owen.d19@gmail.com>
2022-09-29 08:52:26 +0700

    Refactoring insertMovie function to be able to insert as many columns as possible

--- class/movie.php
+++ class/movie.php
@@ -9,7 +9,26 @@ class movie extends orangtua
         parent::__construct();
     }
 
-    public function getMovie($keyword, $offset=null, $limit=null)
+    public function insertMovie($arr_movie)
+    {
+        $arr_col = array();
+        $arr_tanya = array();
+
+        foreach ($arr_movie as $column => $value) {
+            $arr_col[] = $column;
+            $arr_tanya[] = "?";
+        }
+        $sql = "INSERT INTO movie(".implode(",", $arr_col).") VALUES(".implode(",", $arr_tanya).")";
+        $stmt = $this->mysqli->prepare($sql);
+        $stmt->bind_param('ssdsi', $arr_movie["judul"],
+            $arr_movie["rilis"], $arr_movie["skor"],
+            $arr_movie["sinopsis"], $arr_movie["serial"]);
+
+        $stmt->execute();
+        return $stmt->insert_id;
+    }
+
+    public function getMovie($keyword, $offset = null, $limit = null)
     {
         $sql = "SELECT * FROM movie WHERE judul like ?";
         if (!is_null($offset) && !is_null($limit)) {
@@ -21,7 +40,6 @@ class movie extends orangtua
             $stmt->bind_param('sii', $modified_keyword, $offset, $limit);
         } else {
             $stmt->bind_param('s', $modified_keyword);
-        
         }
         $stmt->execute();
         $res = $stmt->get_result();
@@ -29,7 +47,8 @@ class movie extends orangtua
         return $res;
     }
 
-    public function getJumlahData($keyword) {
+    public function getJumlahData($keyword)
+    {
         $res = $this->getMovie($keyword);
         return $res->num_rows;
     }
